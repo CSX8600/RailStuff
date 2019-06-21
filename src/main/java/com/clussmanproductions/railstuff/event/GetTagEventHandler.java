83781dc5fec5d5212f35be72711ca7e45781e71c
RagingLightning <raginglightningcode@gmail.com>
2019-06-21 21:33:47 +0200

    finish IR integration

--- src/main/java/com/clussmanproductions/railstuff/event/GetTagEventHandler.java
+++ src/main/java/com/clussmanproductions/railstuff/event/GetTagEventHandler.java
@@ -1,21 +1,32 @@
 package com.clussmanproductions.railstuff.event;
 
+import cam72cam.immersiverailroading.entity.EntityRollingStock;
 import cam72cam.immersiverailroading.thirdparty.event.TagEvent;
+import com.clussmanproductions.railstuff.ModRailStuff;
 import com.clussmanproductions.railstuff.data.RollingStockIdentificationData;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
+import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.Level;
 
-@Mod.EventBusSubscriber(Side.SERVER)
+@Mod.EventBusSubscriber
 public class GetTagEventHandler {
 
     @SubscribeEvent
     public static void getTag(TagEvent.GetTagEvent e) {
 
-
-
+        ModRailStuff.logger.log(Level.INFO, "GetTag");
+        //World world = DimensionManager.getWorld(DimensionManager.getRegisteredDimensions().get(DimensionType.OVERWORLD).firstInt());
         World world = null;
+        for (World w : DimensionManager.getWorlds()) {
+            if (w.getEntities(EntityRollingStock.class, p -> {return p.getPersistentID().equals(e.stockID);}).size() > 0) {
+                world = w;
+                break;
+            }
+        }
         RollingStockIdentificationData data = RollingStockIdentificationData.get(world);
         e.tag = data.getIdentifierByUUID(e.stockID);
 
--- src/main/java/com/clussmanproductions/railstuff/event/SetTagEventHandler.java
+++ src/main/java/com/clussmanproductions/railstuff/event/SetTagEventHandler.java
@@ -1,24 +1,36 @@
 package com.clussmanproductions.railstuff.event;
 
+import cam72cam.immersiverailroading.entity.EntityRollingStock;
 import cam72cam.immersiverailroading.thirdparty.event.TagEvent;
+import com.clussmanproductions.railstuff.ModRailStuff;
 import com.clussmanproductions.railstuff.data.RollingStockIdentificationData;
 import com.clussmanproductions.railstuff.network.PacketHandler;
 import com.clussmanproductions.railstuff.network.PacketSetIdentifierForClient;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.DimensionType;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.logging.log4j.Level;
 
-@Mod.EventBusSubscriber(Side.SERVER)
+@Mod.EventBusSubscriber
 public class SetTagEventHandler
 {
     @SubscribeEvent
     public static void setTag(TagEvent.SetTagEvent e) {
 
+        ModRailStuff.logger.log(Level.INFO, "GetTag");
+        //World world = DimensionManager.getWorld(DimensionManager.getRegisteredDimensions().get(DimensionType.OVERWORLD).firstInt());
         World world = null;
+        for (World w : DimensionManager.getWorlds()) {
+            if (w.getEntities(EntityRollingStock.class, p -> {return p.getPersistentID().equals(e.stockID);}).size() > 0) {
+                world = w;
+                break;
+            }
+        }
         RollingStockIdentificationData data = RollingStockIdentificationData.get(world);
         data.setIdentifierGivenUUID(e.stockID, e.tag);
 
