e5200786d8d18f1d7b0bd3e77ec305915ff4f821
Christian Mesh <christianmesh1@gmail.com>
2019-11-11 18:35:08 -0500

    Fix hooks into IR code

--- src/main/java/com/clussmanproductions/railstuff/util/ImmersiveRailroadingHelper.java
+++ src/main/java/com/clussmanproductions/railstuff/util/ImmersiveRailroadingHelper.java
@@ -1,7 +1,12 @@
 package com.clussmanproductions.railstuff.util;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import cam72cam.mod.block.tile.TileEntity;
+import cam72cam.mod.entity.ModdedEntity;
+import cam72cam.mod.math.Vec3i;
 import com.clussmanproductions.railstuff.ModRailStuff;
 import com.clussmanproductions.railstuff.tile.SignalTileEntity;
 
@@ -15,12 +20,13 @@ import cam72cam.immersiverailroading.tile.TileRailGag;
 import cam72cam.immersiverailroading.track.BuilderSwitch;
 import cam72cam.immersiverailroading.track.IIterableTrack;
 import cam72cam.immersiverailroading.util.SwitchUtil;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import trackapi.lib.ITrack;
+import trackapi.lib.Util;
 
 public class ImmersiveRailroadingHelper {
 	public static Vec3d findOrigin(BlockPos currentPos, EnumFacing signalFacing, World world)
@@ -35,22 +41,19 @@ public class ImmersiveRailroadingHelper {
 			for(int i = 0; i <= 10; i++)
 			{
 				workingPos = workingPos.offset(searchDirection);
-				
-				if (world.getBlockState(workingPos).getBlock() instanceof BlockRailBase)
+
+				ITrack tile = Util.getTileEntity(world, new Vec3d(workingPos), false);;
+				if (tile == null)
 				{
-					TileRailBase tile = (TileRailBase)world.getTileEntity(workingPos);
-					if (tile == null)
-					{
-						continue;
-					}
-					
-					Vec3d current = new Vec3d(workingPos.getX(), workingPos.getY(), workingPos.getZ());
-					
-					Vec3d center = tile.getNextPosition(current, new Vec3d(0, 0, 0));
-					
-					retVal = new Vec3d(center.x, center.y, center.z);
-					break;
+					continue;
 				}
+
+				Vec3d current = new Vec3d(workingPos.getX(), workingPos.getY(), workingPos.getZ());
+
+				Vec3d center = tile.getNextPosition(current, new Vec3d(0, 0, 0));
+
+				retVal = new Vec3d(center.x, center.y, center.z);
+				break;
 			}
 			
 			workingPos = new BlockPos(currentPos.getX(), workingPos.getY() + 1, currentPos.getZ());
@@ -62,7 +65,7 @@ public class ImmersiveRailroadingHelper {
 	public static Vec3d getNextPosition(Vec3d currentPosition, Vec3d motion, World world, SignalTileEntity.LastSwitchInfo lastSwitchInfo)
 	{
 		BlockPos currentBlockPos = new BlockPos(currentPosition.x, currentPosition.y, currentPosition.z);
-		TileEntity te = world.getTileEntity(currentBlockPos);
+		ITrack te = Util.getTileEntity(world, new Vec3d(currentBlockPos), false);
 		
 		int attempt = 0;
 		while(te == null && attempt < 8)
@@ -98,8 +101,8 @@ public class ImmersiveRailroadingHelper {
 					currentBlockPos = currentBlockPos.up(2);
 					break;
 			}
-			
-			te = world.getTileEntity(currentBlockPos);
+
+			te = Util.getTileEntity(world, new Vec3d(currentBlockPos), false);
 			attempt++;
 		}
 		
@@ -107,82 +110,66 @@ public class ImmersiveRailroadingHelper {
 		{
 			return currentPosition;
 		}
-		
-		TileRailBase railBase = (TileRailBase)te;
-		
-		TileRail railParent = null;
-		TileRail rail = null;
-		
-		if (railBase instanceof TileRail)
-		{
-			railParent = railBase.getParentTile();
-			rail = (TileRail)railBase;
-		}
-		
-		if (rail != null)
-		{
-			if (railParent.info.settings.type != TrackItems.SWITCH)
-			{
-				lastSwitchInfo.lastSwitchPlacementPosition = null;
+
+
+		if (te instanceof TileEntity && ((TileEntity) te).instance() instanceof TileRailBase) {
+			TileRailBase railBase = (TileRailBase) ((TileEntity) te).instance();
+
+			TileRail railParent = null;
+			TileRail rail = null;
+
+			if (railBase instanceof TileRail) {
+				railParent = railBase.getParentTile();
+				rail = (TileRail) railBase;
 			}
-			else if (lastSwitchInfo.lastSwitchPlacementPosition == null || !lastSwitchInfo.lastSwitchPlacementPosition.equals(railParent.info.placementInfo.placementPosition))
-			{
-				// This is a new switch we are encountering - check to make sure
-				// the route is lined in a valid fashion
-				
-				if (currentPosition.distanceTo(railParent.info.placementInfo.placementPosition) <= 0.5)
-				{
-					// We are on the facing point of the switch...we're good to go
-					lastSwitchInfo.lastSwitchPlacementPosition = rail.info.placementInfo.placementPosition;
-				}
-				else
-				{
-					// We are on the trailing point of the switch...we need to make sure we're lined
-					IIterableTrack switchBuilder = (IIterableTrack)railParent.info.getBuilder();
-					
-					boolean isOnStraight = switchBuilder.isOnTrack(railParent.info, currentPosition);
-					SwitchState switchState = SwitchUtil.getSwitchState(rail);
-					
-					if ((isOnStraight && switchState == SwitchState.TURN) ||
-							(!isOnStraight && switchState == SwitchState.STRAIGHT))
-					{
-						// We're incorrectly lined, stop here
-						return currentPosition;
-					}
-					
-					if (switchState != SwitchState.NONE)
-					{
-						// We're correctly lined, ignore the rest of this switch
-						lastSwitchInfo.lastSwitchPlacementPosition = railParent.info.placementInfo.placementPosition;
+
+			if (rail != null) {
+				if (railParent.info.settings.type != TrackItems.SWITCH) {
+					lastSwitchInfo.lastSwitchPlacementPosition = null;
+				} else if (lastSwitchInfo.lastSwitchPlacementPosition == null || !lastSwitchInfo.lastSwitchPlacementPosition.equals(railParent.info.placementInfo.placementPosition)) {
+					// This is a new switch we are encountering - check to make sure
+					// the route is lined in a valid fashion
+
+					if (currentPosition.distanceTo(railParent.info.placementInfo.placementPosition.internal) <= 0.5) {
+						// We are on the facing point of the switch...we're good to go
+						lastSwitchInfo.lastSwitchPlacementPosition = rail.info.placementInfo.placementPosition.internal;
+					} else {
+						// We are on the trailing point of the switch...we need to make sure we're lined
+						IIterableTrack switchBuilder = (IIterableTrack) railParent.info.getBuilder();
+
+						boolean isOnStraight = switchBuilder.isOnTrack(railParent.info, new cam72cam.mod.math.Vec3d(currentPosition));
+						SwitchState switchState = SwitchUtil.getSwitchState(rail);
+
+						if ((isOnStraight && switchState == SwitchState.TURN) ||
+								(!isOnStraight && switchState == SwitchState.STRAIGHT)) {
+							// We're incorrectly lined, stop here
+							return currentPosition;
+						}
+
+						if (switchState != SwitchState.NONE) {
+							// We're correctly lined, ignore the rest of this switch
+							lastSwitchInfo.lastSwitchPlacementPosition = railParent.info.placementInfo.placementPosition.internal;
+						}
 					}
 				}
 			}
+			return railBase.getNextPosition(new cam72cam.mod.math.Vec3d(currentPosition), new cam72cam.mod.math.Vec3d(motion)).internal;
 		}
 		
-		return railBase.getNextPosition(currentPosition, motion);
+		return te.getNextPosition(currentPosition, motion);
 	}
 	
 	public static boolean hasStockNearby(Vec3d currentPosition, World world)
 	{
 		BlockPos currentBlockPos = new BlockPos(currentPosition.x, currentPosition.y, currentPosition.z);
-//		TileEntity te = world.getTileEntity(currentBlockPos);
-//		if (te == null || !(te instanceof TileRailBase))
-//		{
-//			ModRailStuff.logger.info("Could not find rail");
-//			return false;
-//		}
-//		
-//		TileRailBase rail = (TileRailBase)te;
-//		return rail.getStockNearBy(null) != null;
-		
-		AxisAlignedBB bb = new AxisAlignedBB(currentBlockPos.south().west(), currentBlockPos.up(3).east().north());
-		List<EntityMoveableRollingStock> stocks = world.getEntitiesWithinAABB(EntityMoveableRollingStock.class, bb);
-		
-		return !stocks.isEmpty();
-	}
 
-	public static boolean blockPosIsRail(BlockPos pos, World world)
-	{
-		return world.getBlockState(pos).getBlock() instanceof BlockRailBase;
+		AxisAlignedBB bb = new AxisAlignedBB(currentBlockPos.south().west(), currentBlockPos.up(3).east().north());
+		List<EntityMoveableRollingStock> stocks = world.getEntitiesWithinAABB(ModdedEntity.class, bb)
+				.stream()
+				.map(x -> x.getSelf() instanceof EntityMoveableRollingStock ? (EntityMoveableRollingStock)x.getSelf() : null)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+
+		return !stocks.isEmpty();
 	}
 }
